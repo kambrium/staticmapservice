59a9ee53667be371fd51b44a0dae51d19d9daa9c
kambrium <kambrium@users.noreply.github.com>
2018-10-28 10:10:59 +0100

    Added multi marker functionality

--- staticmapservice.py
+++ staticmapservice.py
@@ -10,16 +10,23 @@ def create_map():
         width = int(request.args.get('w'))
         height = int(request.args.get('h'))
         zoom = int(request.args.get('z'))
-        marker_lat = float(request.args.get('mlat'))
-        marker_lon = float(request.args.get('mlon'))
-        marker_color = request.args.get('mcol')
-        marker_size = int(request.args.get('msiz'))
 
         m = StaticMap(width, height, url_template='http://a.tile.osm.org/{z}/{x}/{y}.png')
-        marker = CircleMarker((marker_lon, marker_lat), marker_color, marker_size)
-        m.add_marker(marker)
+
+        if 'markers' in request.args:
+            for marker in request.args.getlist('markers'):
+                marker_properties = dict(item.split(':') for item in marker.split('|'))
+                marker_lat = float(marker_properties['lat'])
+                marker_lon = float(marker_properties['lon'])
+                marker_color = marker_properties['color']
+                marker_size = int(marker_properties['size'])
+                marker_object = CircleMarker((marker_lon, marker_lat), marker_color, marker_size)
+                m.add_marker(marker_object)
+
         image = m.render(zoom=zoom)
+
         return serve_image(image)
+
     except:
         return ('Something went wrong during the creation of your map. Please check your query parameters. A working example is /?w=400&h=300&z=7&mlat=48.2&mlon=11.2&msiz=12&mcol=%23CD0000.')
 
