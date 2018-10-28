cdd9f42872dd9dbb85f3088185a986ee2f5e096a
kambrium <kambrium@users.noreply.github.com>
2018-10-28 13:31:28 +0100

    Added line functionality

--- staticmapservice.py
+++ staticmapservice.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, send_file
 from io import BytesIO
-from staticmap import StaticMap, CircleMarker
+from staticmap import CircleMarker, Line, StaticMap
 
 app = Flask(__name__)
 
@@ -22,6 +22,20 @@ def create_map():
                 marker_size = int(marker_properties['size'])
                 marker_object = CircleMarker((marker_lon, marker_lat), marker_color, marker_size)
                 m.add_marker(marker_object)
+
+        if 'lines' in request.args:
+            for line in request.args.getlist('lines'):
+                line_properties = dict(item.split(':') for item in line.split('|'))
+                line_coordinates = []
+                for coord in line_properties['coords'].split(';'):
+                    line_coordinate = []
+                    line_coordinate.append(float(coord.split(',')[1]))
+                    line_coordinate.append(float(coord.split(',')[0]))
+                    line_coordinates.append(line_coordinate)
+                line_color = line_properties['color']
+                line_size = int(line_properties['size'])
+                line_object = Line(line_coordinates, line_color, line_size)
+                m.add_line(line_object)
 
         image = m.render(zoom=zoom)
 
